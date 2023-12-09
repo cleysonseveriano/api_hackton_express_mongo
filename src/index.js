7d523316fae2c50c1a7cfabc881a5edb8c52f512
cleysonseveriano <cleyson.tab@gmail.com>
2023-12-09 10:10:50 -0300

    init

---
+++ src/index.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+app.use(express.json());
+
+const User = mongoose.model('User', {
+    nome: String,
+    email: String,
+    senha: String
+});
+
+// Verbos HTTP
+app.get('/', async (req,res) => {
+    const usuarios = await User.find();
+    res.send(usuarios);
+});
+
+app.delete("/:id", async (req,res) => {
+    const usuario = await User.findByIdAndDelete(req.params.id);
+    return res.send(usuario);
+});
+
+app.put('/:id', async (req,res) => {
+    const usuario = await User.findByIdAndUpdate(req.params.id, {
+        nome: req.body.nome,
+        email: req.body.email,
+        senha: req.body.senha
+    }, {
+        new: true
+    });
+    return res.send(usuario);
+});
+
+app.post('/', async (req,res) => {
+    const usuario = new User({
+        nome: req.body.nome,
+        email: req.body.email,
+        senha: req.body.senha
+    });
+
+    await usuario.save();
+    res.send(usuario);
+});
+
+const PORT = 5000;
+
+const DB_PASSWORD = '';
+app.listen(PORT, () => {
+    mongoose.connect(`mongodb+srv://cleysontab:${DB_PASSWORD}@appwebhackaton.sswddix.mongodb.net/?retryWrites=true&w=majority`);
+    console.log('App is running...')
+});
